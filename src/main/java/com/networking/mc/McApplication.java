84a81760e00ab851d505b5024833a2abd5b01366
chetan2510 <chetanchadha2594@gmail.com>
2021-01-04 15:04:39 +0530

    Added basic structure

---
+++ src/main/java/com/networking/mc/McApplication.java
@@ -0,0 +1,13 @@
+package com.networking.mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McApplication.class, args);
+    }
+
+}
