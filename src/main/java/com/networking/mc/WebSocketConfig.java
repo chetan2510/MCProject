8e0704a0d518974ccf2001599438c174ec961de4
chetan2510 <chetanchadha2594@gmail.com>
2021-03-06 18:56:02 +0100

    Added websocket code

--- src/main/java/com/networking/mc/WebSocketConfig.java
+++ src/main/java/com/networking/mc/WebSocketConfig.java
@@ -1,17 +1,22 @@
 package com.networking.mc;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer  {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry)
     {
 
-        registry.addEndpoint("/testchat").setAllowedOrigins("http://localhost:4200").withSockJS();
+        registry.addEndpoint("/testchat")
+                .setAllowedOriginPatterns("http://*:4200")
+                .withSockJS();
     }
 
     @Override
