7d2f9d668c1525575fa632de675a7e26020c14dd
chetan2510 <chetanchadha2594@gmail.com>
2021-03-08 12:31:07 +0100

    removed the bug

--- src/main/java/com/networking/mc/service/RescueService.java
+++ src/main/java/com/networking/mc/service/RescueService.java
@@ -30,7 +30,6 @@ public class RescueService {
      * @return
      */
     public String addRescuerToList(RescueModel rescueModel) {
-        rescueModel.password = "qwertyuiop";
             rescuerRepository.save(rescueModel);
             return "Rescuer added to the list, please sign in to continue";
     }
